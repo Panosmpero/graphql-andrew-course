7121c45f4e8bd3b644c962c3fe63e875d7ed317e
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-04-27 02:00:38 +0100

    scalar types

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import { GraphQLServer } from "graphql-yoga";
 
+// Scalar types
+// ====================
+// String, Boolean, Int, Float, ID
+
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    hello: String!
+    id: ID!
+    name: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return "This is my first query!";
+    id() {
+      return "34r3r34f3";
+    },
+    name() {
+      return "Beros!";
+    },
+    age() {
+      return 34;
+    },
+    employed() {
+      return false;
+    },
+    gpa() {
+      return 2.14; // can return null as we did not put ! on typeDefs
     },
   },
 };
