f9cf4ddc50e7a674a15af74282b45d134c7a7e87
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-04-27 01:48:37 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { GraphQLServer } from "graphql-yoga";
+
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query!";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+server.start(() => console.log("Server running!"));
